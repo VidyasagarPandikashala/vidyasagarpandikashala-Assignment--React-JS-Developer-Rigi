e8b68a93aeeebfff5c95dab83470ecacfaa2bc32
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2024-02-16 16:17:10 +0530

    [APIs] - created fetch api for
    -- to get list of videos in assets

---
+++ vplayer-frontend/src/APIs/getVideoList.js
@@ -0,0 +1,15 @@
+const listOFVideos = [
+  {
+    id: 1,
+    videoSrc: "./assets/apjkalam1.mp4",
+    name: "apjkalam1",
+  },
+
+  { id: 2, videoSrc: "./assets/video1.mp4", name: "video1" },
+];
+
+function getVideoList() {
+  return listOFVideos;
+}
+
+export default getVideoList;
