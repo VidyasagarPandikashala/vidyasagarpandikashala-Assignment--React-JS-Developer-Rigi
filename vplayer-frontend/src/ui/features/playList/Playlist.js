2f9b84fa65eb7134cc98048be5a0f0309a96e831
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2024-02-18 15:58:40 +0530

    [Playlist.js] : Created Playlist page-utilizes the playlistcard component

--- vplayer-frontend/src/ui/features/playList/Playlist.js
+++ vplayer-frontend/src/ui/features/playList/Playlist.js
@@ -1,11 +1,37 @@
-import PlayListCard from "../../shared-components/PlaylistCard/PlayListCard";
+import { useSelector } from "react-redux";
+import PlayListCard from "../../shared-components/playlistCard/PlayListCard";
 
-function playlist() {
+import CreatePlayList from "../../shared-components/dialog-popup/createplaylist/CreatePlayList";
+import { SELECT_TABLE } from "../../../redux/applicationslice/appSlice";
+import { Link } from "react-router-dom";
+import styles from "./Playlist.module.css";
+
+function Playlist() {
+  const selectPlayList = useSelector(SELECT_TABLE.selectPlayList);
+  console.log(selectPlayList);
   return (
     <>
-      <PlayListCard />
+      <div className={styles.noPlaylistBackground}>
+        {selectPlayList.length === 0 ? (
+          <div className={styles.noPlaylist}> No playlist available</div>
+        ) : (
+          selectPlayList.map((eachPlaylist) => {
+            return (
+              <div key={eachPlaylist.playListId}>
+                <Link
+                  to={`/playlist/${eachPlaylist.playListId}`}
+                  className={styles.linkStyle}
+                >
+                  <PlayListCard playlistName={eachPlaylist.playlistName} />
+                </Link>
+              </div>
+            );
+          })
+        )}
+      </div>
+      <CreatePlayList className={styles.createPlaylist} />
     </>
   );
 }
 
-export default playlist;
+export default Playlist;
