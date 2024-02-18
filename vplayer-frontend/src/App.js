044c7b24aa1a425c88dcc804b5e72bb247a9c06e
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2024-02-18 15:47:28 +0530

    [index.js,app.js]configured url path for navigation
    page route have been configrured and tagged in app and index

--- vplayer-frontend/src/App.js
+++ vplayer-frontend/src/App.js
@@ -1,11 +1,29 @@
 import HomePage from "./ui/features/homepage/HomePage";
+import Playlist from "./ui/features/playList/Playlist";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PlaylistVideoList from "./ui/features/playlist-videoslist/PlaylistVideoList";
+import Root from "./Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "playlist", element: <Playlist /> },
+      { path: "playlist/:playlistId", element: <PlaylistVideoList /> },
+    ],
+  },
+]);
 
 function App() {
-  let videos = []; // if recieved from backend application otherwise uses stub assets data in th homepage
+  let videos = [];
   return (
-    <div>
-      <HomePage data={videos}></HomePage>
-    </div>
+    <RouterProvider router={router}>
+      <div>
+        <HomePage data={videos}></HomePage>
+      </div>
+    </RouterProvider>
   );
 }
 export default App;
