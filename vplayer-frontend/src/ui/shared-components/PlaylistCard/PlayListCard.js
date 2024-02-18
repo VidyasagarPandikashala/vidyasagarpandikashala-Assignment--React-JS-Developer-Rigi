665772c6808532dff7f30b6ced44321cd6ceb8cf
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2024-02-18 15:57:21 +0530

    [playlistCard.js] : displays the playlist name and routes it to specific page

--- vplayer-frontend/src/ui/shared-components/PlaylistCard/PlayListCard.js
+++ vplayer-frontend/src/ui/shared-components/PlaylistCard/PlayListCard.js
@@ -1,16 +1,13 @@
-import VideoPlayerThumbnail from "../VideoPlayerThumbnail/VideoPlayerThumbnail";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./PlayListCard.module.css";
 import { faWindowMinimize } from "@fortawesome/free-solid-svg-icons";
-function PlayListCard() {
+import PlaylistThumbnail from "../playlistThumbnail/PlaylistThumbnail";
+function PlayListCard({ playlistName }) {
   return (
     <div className={styles.playlistStyle}>
-      <VideoPlayerThumbnail thumbnailStyle={"thumbnailcontainer"} />
-      <div>
-        <p>PlaylistName</p>
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faWindowMinimize} />
+      <PlaylistThumbnail />
+      <div className={styles.paragraph}>
+        <p>{playlistName}</p>
       </div>
     </div>
   );
