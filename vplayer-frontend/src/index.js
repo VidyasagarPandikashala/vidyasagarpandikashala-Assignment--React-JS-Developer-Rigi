c3a8bb580683bf76a940b2666c5134da77a3698a
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2024-02-18 19:54:35 +0530

    enableMapSet from immer while application starts

--- vplayer-frontend/src/index.js
+++ vplayer-frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { store } from "./redux/store/store";
+import store from "../src/redux/store/store";
 import { Provider } from "react-redux";
+import { enableMapSet } from "immer";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+enableMapSet();
+
 root.render(
   <Provider store={store}>
     <App />
